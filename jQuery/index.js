2e5f1082eda6fd94e9c5d86278492e596d5e3369
julian saldana <julianesaldana@yahoo.com>
2023-04-07 16:08:06 -0700

    progress

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,4 @@
 // using jquery, $() represents document
 // changing color property to red
-$("h1").css("color", "red");
+$("h1").css("color", "red");    // $("h1") compared to document.querySelector("h1")
 
